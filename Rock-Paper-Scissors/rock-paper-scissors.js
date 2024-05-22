7ee76cc7dd4b53a5680b16a97314345676507a22
TylerP123456789 <113116228+TylerP123456789@users.noreply.github.com>
2024-05-22 13:26:09 -0400

    Add files via upload

---
+++ Rock-Paper-Scissors/rock-paper-scissors.js
@@ -0,0 +1,72 @@
+let computer = '';
+function choice(choice) {
+    computerChoice();
+    if (choice === 'rock' && computer === 'rock') {
+        document.getElementById('displayPlayer').innerHTML = 'Rock';
+        document.getElementById('displayComputer').innerHTML = 'Rock';
+        document.getElementById('displayResult').innerHTML = 'Tie';
+    }
+    if (choice === 'rock' && computer === 'paper') {
+        document.getElementById('displayPlayer').innerHTML = 'Rock';
+        document.getElementById('displayComputer').innerHTML = 'Paper';
+        document.getElementById('displayResult').innerHTML = 'Computer Wins';
+    }
+    if (choice === 'rock' && computer === 'scissors') {
+        document.getElementById('displayPlayer').innerHTML = 'Rock';
+        document.getElementById('displayComputer').innerHTML = 'Scissors';
+        document.getElementById('displayResult').innerHTML = 'Player Wins';
+    }
+    if (choice === 'scissors' && computer === 'scissors') {
+        document.getElementById('displayPlayer').innerHTML = 'Scissors';
+        document.getElementById('displayComputer').innerHTML = 'Scissors';
+        document.getElementById('displayResult').innerHTML = 'Tie';
+    }
+    if (choice === 'scissors' && computer === 'rock') {
+        document.getElementById('displayPlayer').innerHTML = 'Scissors';
+        document.getElementById('displayComputer').innerHTML = 'Rock';
+        document.getElementById('displayResult').innerHTML = 'Computer Wins';
+    }
+    if (choice === 'scissors' && computer === 'paper') {
+        document.getElementById('displayPlayer').innerHTML = 'Scissors';
+        document.getElementById('displayComputer').innerHTML = 'Paper';
+        document.getElementById('displayResult').innerHTML = 'Player Wins';
+    }
+    if (choice === 'paper' && computer === 'paper') {
+        document.getElementById('displayPlayer').innerHTML = 'Paper';
+        document.getElementById('displayComputer').innerHTML = 'Paper';
+        document.getElementById('displayResult').innerHTML = 'Tie';
+    }
+    if (choice === 'paper' && computer === 'scissors') {
+        document.getElementById('displayPlayer').innerHTML = 'Paper';
+        document.getElementById('displayComputer').innerHTML = 'Scissors';
+        document.getElementById('displayResult').innerHTML = 'Computer Wins';
+    }
+    if (choice === 'paper' && computer === 'rock') {
+        document.getElementById('displayPlayer').innerHTML = 'Paper';
+        document.getElementById('displayComputer').innerHTML = 'Rock';
+        document.getElementById('displayResult').innerHTML = 'Player Wins';
+    }
+
+
+
+
+}
+
+function computerChoice() {
+    let int;
+
+        int = Math.floor(Math.random() * 3) + 1;
+        if(int === 1){
+            computer = 'rock';
+        }
+
+        else if(int === 2){
+            computer = 'paper';
+        }
+        else if(int === 3){
+            computer = 'scissors';
+        }
+
+
+}
+
