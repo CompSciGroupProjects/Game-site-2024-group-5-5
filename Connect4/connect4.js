84551864724c889e86aa19c779e0ce8a14aa7430
126CC <113187762+126CC@users.noreply.github.com>
2024-05-28 13:18:38 -0400

    adding nav

--- Connect4/connect4.js
+++ Connect4/connect4.js
@@ -159,3 +159,12 @@ function checkDiagonal(row, col) {
 function newGame() {
     location.reload();
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
